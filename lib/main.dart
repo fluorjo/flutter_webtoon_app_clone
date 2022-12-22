df38d9f95081a21637084bddf8193513dd38c876
fluorjo <fluorjo@naver.com>
2022-12-22 19:45:40 +0900

    4.4 Widget Lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,21 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:ThemeData(
+      theme: ThemeData(
         textTheme: TextTheme(
-          titleLarge:TextStyle(
-            color:Colors.red,
+          titleLarge: TextStyle(
+            color: Colors.red,
           ),
         ),
       ),
@@ -27,7 +34,8 @@ class _AppState extends State<App> {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            MyLargeTitle(),
+            showTitle ? MyLargeTitle() : const Text('nothing'),
+            IconButton(onPressed: toggleTitle, icon: Icon(Icons.remove_red_eye))
           ],
         )),
       ),
@@ -35,16 +43,42 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print('init state');
+  }
+  //init state는 항상 build보다 먼저 실행된다.
+  //변수를 초기화하고 API업데이트를 구독할 수 있게 한다.
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+  //dispose:위젯이 스크린에서 제거될 때 호출되는 메소드.
+  //API 업데이트나 이벤트 리스너로부터 구독을 취소하거나
+  //form의 리스너로부터 벗어나고 싶을 때 사용 가능.
+  //무언가를 취소할 때 사용. 위젯이 위젯 트리에서 제거되기 전에 뭔가를 취소한다거나.
+
   Widget build(BuildContext context) {
+    print('build');
     return Text(
       'My Large Title',
-      style: TextStyle(fontSize: 30,color:Theme.of(context).textTheme.titleLarge?.color,),
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
     );
   }
 }
