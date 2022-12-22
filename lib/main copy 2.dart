51f4984a5e1809127c515eca4ab5eaac25874834
fluorjo <fluorjo@naver.com>
2022-12-22 19:11:13 +0900

    4.3 BuildContext

--- lib/main copy 2.dart
+++ lib/main copy 2.dart
@@ -10,41 +10,41 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:ThemeData(
-        textTheme: TextTheme(
-          titleLarge:TextStyle(
-            color:Colors.red,
-          ),
-        ),
-      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFf4eddb),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            MyLargeTitle(),
+            const Text(
+              'Click Count',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              '$counter',
+              style: const TextStyle(fontSize: 30),
+            ),
+            IconButton(
+              iconSize: 40,
+              onPressed: onClicked,
+              icon: const Icon(
+                Icons.add_box_rounded,
+              ),
+            ),
           ],
         )),
       ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatelessWidget {
-  const MyLargeTitle({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(fontSize: 30,color:Theme.of(context).textTheme.titleLarge?.color,),
     );
   }
 }
