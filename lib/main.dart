4b7d681e45b98062509d6e5d39c9dc8c5030d27a
fluorjo <fluorjo@naver.com>
2022-12-24 03:06:47 +0900

    6.6 FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
