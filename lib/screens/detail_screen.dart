66f224b42d176f9c37978226e660918e0dbd6a58
fluorjo <fluorjo@naver.com>
2022-12-25 08:59:19 +0900

    6.13 Futures

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
   const DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +39,8 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         centerTitle: true,
         title: Text(
-          title,
+          widget.title,
+          //stateful이 되면서 widget을 적어주게 됐다. 여기서 위젯은 부모=디테일 스크린, 스테이트풀위젯으로 가라는 뜻.
           style: const TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.w500,
@@ -35,7 +55,7 @@ class DetailScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Hero(
-              tag:id,
+              tag: widget.id,
               child: Container(
                 width: 200,
                 clipBehavior: Clip.hardEdge,
@@ -48,7 +68,7 @@ class DetailScreen extends StatelessWidget {
                         color: Colors.black.withOpacity(0.5),
                       )
                     ]),
-                child: Image.network(thumb),
+                child: Image.network(widget.thumb),
               ),
             ),
           ],
