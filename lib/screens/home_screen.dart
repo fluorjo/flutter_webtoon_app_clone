f8c61e38668c257e63dfe83e243df5993d6e3380
fluorjo <fluorjo@naver.com>
2022-12-25 01:36:14 +0900

    6.8 Webtoon Card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,21 +28,63 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                //10개씩만 불러오고 스크롤 할 때마다 추가적으로 불러온다. 
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) =>SizedBox(width: 20,),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: makeList(snapshot))
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        //10개씩만 불러오고 스크롤 할 때마다 추가적으로 불러온다.
+        return Column(
+          children: [
+            Container(
+              width: 200,
+              clipBehavior: Clip.hardEdge,
+              decoration:
+                  BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        offset: Offset(10,10),
+                        color: Colors.black.withOpacity(0.5),
+                      )
+                    ]),
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
